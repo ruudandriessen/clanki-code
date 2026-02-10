56de7ea609f53ab558fcac07cec2e0876e758da3
Ruud Andriessen <ruud.andriessen@gmail.com>
2026-02-10 16:59:28 +0100

    feat(frontend): migrate sidebar to shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
+import { TooltipProvider } from "./components/ui/tooltip";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <TooltipProvider>
+      <RouterProvider router={router} />
+    </TooltipProvider>
   </StrictMode>,
 );
